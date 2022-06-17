0452c23947ae4b17eea5245fd8d01db22f8f6aeb
ones1kk <ones1k95@gmail.com>
2022-06-18 01:11:58 +0900

    ✨ Add : Entity, Repository, Service

---
+++ src/main/java/com/onesik/jwttutorial/dto/TokenDto.java
@@ -0,0 +1,12 @@
+package com.onesik.jwttutorial.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDto {
+    private String token;
+}
---
+++ src/main/java/com/onesik/jwttutorial/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.onesik.jwttutorial.repository;
+
+import com.onesik.jwttutorial.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByUsername(String username);
+}
